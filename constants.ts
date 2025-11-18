20e2fa587c19d4d2fc409784db13e6353dbdfe67
nikolaspg74-png <nikolaspg74@gmail.com>
2025-11-18 09:55:09 -0300

    correcting

--- constants.ts
+++ constants.ts
@@ -3,4 +3,4 @@
 
 export const API_URL = window.location.hostname === 'localhost'
   ? 'http://localhost:3000'
-  : 'lettie-nonsegregative-isabella.ngrok-free.dev';
+  : 'https://lettie-nonsegregative-isabella.ngrok-free.dev';
