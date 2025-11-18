822b990855e5f50a4a9688752dfa6dfe05faedb2
nikolaspg74-png <nikolaspg74@gmail.com>
2025-11-18 09:42:39 -0300

    fix

--- constants.ts
+++ constants.ts
@@ -1,2 +1,6 @@
+// Mude para false quando for fazer deploy
 
-export const API_URL = "https://task.loca.lt";
+
+export const API_URL = window.location.hostname === 'localhost'
+  ? 'http://localhost:3000'
+  : 'https://task.loca.lt';
